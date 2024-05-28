9f2c200db5b215d3d18bb77e6066f6015e53e034
Amir Kavousian <kav.amir@gmail.com>
2024-05-28 11:29:28 -0700

    Update python installation and package management to use pyenv and hatch

--- onboarding/02_setup_python/03_setup_python_venv_management.sh
+++ onboarding/02_setup_python/03_setup_python_venv_management.sh
@@ -1,5 +1,6 @@
 # I use PyEnv to manage multiple python installations and virtual environments.
 
+### Install PyEnv
 # Follow instructions here to install PyEnv: https://github.com/pyenv/pyenv
 brew install pyenv
 brew install pyenv-virtualenv
@@ -13,7 +14,7 @@ exec "$SHELL"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-###
+### Install Python
 # You can now install python versions using pyenv
 pyenv install 3.10.0
 
@@ -23,25 +24,44 @@ pyenv install -l
 # Get the list of python versions that are installed by pyenv on your machine
 pyenv versions
 
-# You can now create virtual environments using pyenv
-# Note that pyenv stores the virtual envs in a central directory, not inside the project directory.
-# So if you plan to create separate venvs for different repositories, include the repo name in the venv name.
+### Create Virtual Env
+# Create virtual environments using pyenv (option a)
+# Pyenv stores the virtual envs in a central directory, not inside the project directory.
+# If you plan to create separate venvs for different repositories, include the repo name in the venv name.
+# But, pyenv still creates the venv inside the puyenv-root directory (you can't specify the path in the venv name).
+# If you want to create the virtual env inside your project directory, you need a different workflow (read option b)
 pyenv virtualenv 3.10.0 my-virtual-env
 
+# Create virtual environments using pyenv (option b)
+# If you want to create the venv directory inside your project repo, and have pyenv manage it, you need to:
+# i. create a venv inside any directory you want
+cd $PROJ_DIR
+python -m venv ./.venv
+# ii. link the venv to an available pyenv python installation
+ln -s ./.venv ~/.pyenv/versions/my-virtual-env
+
+### Use Virtual Envs
 # You can now activate the virtual environment using pyenv
 pyenv activate my-virtual-env
 
 # You can now deactivate the virtual environment using pyenv
 pyenv deactivate
 
-# You can now delete the virtual environment using pyenv
+# To delete the virtual environment using pyenv
 pyenv uninstall my-virtual-env
 
-# You can now delete the python version using pyenv
+# To delete the python version using pyenv
 pyenv uninstall 3.10.0
 
 # You can switch between python versions using pyenv
-# `pyenv global` reads/writes the ~/.python-version file. In the absence of any other clues, pyenv uses this file to determine which python version to use.
-# `pyenv shell` sets the PYENV_VERSION environment variable for that shell. AKA it's equivalent to export PYENV_VERSION=3.9.1 for example. That means that this is temporary - when the shell exits, this command is lost until next time.
+# To automatically activate local env when you enter your local development folder of your project, you have two options:
+# (a) `pyenv global` reads/writes the ~/.python-version file. In the absence of any other clues, pyenv uses this file to determine which python version to use.
+# (b) `pyenv local` selects what Python version to use in the current directory or its subdirectories.
+# (c) `pyenv shell` sets the PYENV_VERSION environment variable for that shell. AKA it's equivalent to export PYENV_VERSION=3.9.1 for example. That means that this is temporary - when the shell exits, this command is lost until next time.
 pyenv global 3.10.0
 pyenv local 3.10.0
+
+# More on using Pyenv with virtual envs:
+# - https://medium.com/codex/python-version-management-with-pyenv-and-pyenv-virtualenv-linux-ecd6578b7bbf
+# - https://github.com/pyenv/pyenv-virtualenv#activate-virtualenv
+# - https://stackoverflow.com/questions/77338439/is-there-a-difference-between-pyenv-activate-and-pyenv-local
--- onboarding/02_setup_python/README.md
+++ onboarding/02_setup_python/README.md
@@ -1,4 +1,20 @@
 # Python installation
 
-- Install Python via Homebrew on your machine. 
-- Install Poetry for package management of Python projects.
+## Old setup (Poetry)
+
+- Install Python via `Homebrew` on your machine.
+- Specify dependencies in `pyproject.toml` file.
+- Install `Poetry` for managing virtual environments, installing the project, and creating build files.
+
+## New setup (pyenv/pip/hatch)
+
+The main difference is that this workflow does not have a "lock" file (no `poetry.lock` or `pipenv.lock` file). 
+> hatch doesn't currently implement lock files. The lock file mechanism has been non-standard for years and specific to the dependency management system you choose (pip vs. pipenv vs. poetry), but they're now working on a standard, ecosystem-wide solution, and hatch will then implement that.
+For now, if you need this for some reason, look at the Dockerfile for how I'm generating a requirements.txt file, which I believe has all the requirements versioned. This is the workaround/stopgap by hatch.
+we don't need to lock dependency versions beyond generating iterations of Docker images. Once we know an image passes CI tests, we tag it. That's all.
+
+- Install Python using `Pyenv`.
+- Manage virtual environments via `Pyenv` (see caveats) or directly using `venv` (you can link `pyenv` to these virtual environments to use `pyenv` shell commands to activate / deactivate the virtual envs)
+- Specify dependencies in `pyproject.toml` file.
+- Install the project using `pip`. I.e., `pip install -e .`
+- Use `Hatch` to build wheel packages (create build files).
